c175900303f3c8328f02b2469bd09b7a463b15ee
Daan Schoone <d.schoone@presspage.com>
2024-10-02 21:19:10 +0200

    feat: add line number HL for PHP and Go files
    To better distinguish between projects using PHP and Go I made the line
    numbers colored by the color of the languages.

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,6 @@
+local lineHL = { bg = '#00ADD8' }
+local cursorHL = { bg = '#5DC9E2', fg = '#16181A' }
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', lineHL)
+vim.api.nvim_set_hl(0, 'CursorLineNr', cursorHL)
+vim.api.nvim_set_hl(0, 'LineNrBelow', lineHL)
---
+++ after/ftplugin/php.lua
@@ -0,0 +1,6 @@
+local lineHL = { bg = '#777BB3' }
+local cursorHL = { bg = '#AEB2D5', fg = '#16181A' }
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', lineHL)
+vim.api.nvim_set_hl(0, 'CursorLineNr', cursorHL)
+vim.api.nvim_set_hl(0, 'LineNrBelow', lineHL)
